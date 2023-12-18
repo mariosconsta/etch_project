69322e431ff2e88a14860cdb3aeb4e6586ae7cb9
Marios Constantinou <mariosconsta@gmail.com>
2023-12-18 18:06:44 +0200

    Can target each individual div can generate a random HEX color can paint each div as the cursor hovers over it
    Minor stuff

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const mainContainer = document.getElementById('squaresContainer');
-console.log(`main container: ${mainContainer}`)
+
 
 
 function createRowDiv() {
     const rowDivContainer = document.createElement('div');
     rowDivContainer.setAttribute('id', 'rowDivContainer');
-    console.log(`rowDivContainer: ${rowDivContainer}`)
 
     for (let k = 0; k < 17; k++) {
         let interactiveDiv = document.createElement('div');
@@ -15,12 +14,43 @@ function createRowDiv() {
     mainContainer.appendChild(rowDivContainer);
 }
 
-
 function appendRowDiv() {
     for (let i = 0; i < 17; i++) {
         createRowDiv()
     }
+
 }
 
-// createRowDiv()
+function getRandomColor() {
+    // Generate random values for Red, Green, and Blue components
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    // Convert decimal to hexadecimal and ensure two digits
+    var hexR = r.toString(16).padStart(2, '0');
+    var hexG = g.toString(16).padStart(2, '0');
+    var hexB = b.toString(16).padStart(2, '0');
+
+    // Concatenate the components to form the final Hex color
+    var hexColor = '#' + hexR + hexG + hexB;
+
+    return hexColor;
+}
 appendRowDiv()
+
+
+console.log()
+// const interactiveDivs = mainContainer.querySelectorAll('#rowDivContainer').querySelectorAll('.interactiveDiv')
+// let interactiveRowDivs = document.querySelector('#rowDivContainer')
+
+mainContainer.addEventListener('mouseover', (e) => {
+    let target = e.target
+
+    // console.log(target.classList)
+    if (target.classList.contains('interactiveDiv')) {
+        console.log('weeee')
+        let randomColor = getRandomColor()
+        target.setAttribute('style', `background-color:${randomColor}`)
+    }
+})
