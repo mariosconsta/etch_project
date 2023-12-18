757cda8ec4ece36cca53ac5a80ad44c9884c92e9
Marios Constantinou <mariosconsta@gmail.com>
2023-12-18 20:07:03 +0200

    Can increase and decrease grid size

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const mainContainer = document.getElementById('squaresContainer');
+const gridSizeLabel = document.getElementById('gridSizeLabel');
+let gridSizeElem = document.createElement('b');
+gridSizeElem.textContent = 16;
+gridSizeLabel.appendChild(gridSizeElem);
 
+let gridSize = parseInt(gridSizeElem.textContent)
 
-
-function createRowDiv() {
+function createRowDiv(gridSize) {
     const rowDivContainer = document.createElement('div');
-    rowDivContainer.setAttribute('id', 'rowDivContainer');
+    rowDivContainer.setAttribute('class', 'rowDivContainer');
 
-    for (let k = 0; k < 17; k++) {
+    for (let k = 0; k < gridSize; k++) {
+        // console.log(k)
         let interactiveDiv = document.createElement('div');
         interactiveDiv.setAttribute('class', 'interactiveDiv');
         rowDivContainer.appendChild(interactiveDiv);
@@ -14,9 +19,9 @@ function createRowDiv() {
     mainContainer.appendChild(rowDivContainer);
 }
 
-function appendRowDiv() {
-    for (let i = 0; i < 17; i++) {
-        createRowDiv()
+function appendRowDiv(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        createRowDiv(gridSize)
     }
 
 }
@@ -37,20 +42,41 @@ function getRandomColor() {
 
     return hexColor;
 }
-appendRowDiv()
 
+function clearGrid() {
+    [...mainContainer.childNodes].forEach(element => {
+        console.log(element)
+        if (element.tagName === 'DIV') {
+            element.parentNode.removeChild(element)
+        }
+    });
+}
+appendRowDiv(gridSize = gridSize)
 
-console.log()
-// const interactiveDivs = mainContainer.querySelectorAll('#rowDivContainer').querySelectorAll('.interactiveDiv')
-// let interactiveRowDivs = document.querySelector('#rowDivContainer')
 
 mainContainer.addEventListener('mouseover', (e) => {
     let target = e.target
 
-    // console.log(target.classList)
     if (target.classList.contains('interactiveDiv')) {
-        console.log('weeee')
         let randomColor = getRandomColor()
         target.setAttribute('style', `background-color:${randomColor}`)
     }
+})
+
+const increaseGridSizeBtn = document.getElementById('increaseSizeBtn')
+increaseGridSizeBtn.addEventListener('click', (e) => {
+    clearGrid()
+    gridSize = gridSize + 2;
+    gridSizeElem.textContent = gridSize;
+    console.log(gridSize)
+    appendRowDiv(gridSize = gridSize)
+})
+
+const decreaseGridSizeBtn = document.getElementById('decreaseSizeBtn')
+decreaseGridSizeBtn.addEventListener('click', (e) => {
+    clearGrid()
+    gridSize = gridSize - 2;
+    gridSizeElem.textContent = gridSize;
+    console.log(gridSize)
+    appendRowDiv(gridSize = gridSize)
 })
